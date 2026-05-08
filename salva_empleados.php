47293b5a47dd63709f3ca27da212aa3c05944115
madrallica <gabrielasarahiorozcomadrigal@gmail.com>
2026-05-07 18:08:28 -0600

    replace ape/apellido

--- salva_empleados.php
+++ salva_empleados.php
@@ -1,14 +1,14 @@
 <?php
 include "conecta.php";
 $nombre = $_POST['nombre'];
-$ape = $_POST['ape'];
+$apellido = $_POST['apellido'];
 $correo = $_POST['correo'];
 $rol    = $_POST['rol'];
-$pasw   = $_POST['pasw'];
-$pasw_enc = md5($pasw); 
+$password   = $_POST['password'];
+$pasw_enc = md5($password); 
 
 $sql = "INSERT INTO empleados (name, ape, correo, rol, pasw, status) 
-        VALUES ('$nombre', '$ape', '$correo', '$rol', '$pasw_enc', 1)";
+        VALUES ('$nombre', '$apellido', '$correo', '$rol', '$pasw_enc', 1)";
 
 if ($conexion->query($sql) === TRUE) {
     echo "ok"; 
